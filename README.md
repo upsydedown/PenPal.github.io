a105f4e5605b5fad0f115cd44e138ab8c93f1147
upsydedown <chintansenapati@gmail.com>
2024-02-29 20:05:05 +0530

    Update 2.0

--- README.md
+++
@@ -1,3 +0,0 @@
-# NotesApp.github.io
-Developed a sticky-notes' webapp using HTML, CSS, JavaScript & LocalStorage.
-
--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const addButton = document.querySelector("#add");
 const updateLSData = () => {
-    const textAreaData = document.querySelectorAll('textarea');
-    const notes = [];
-    // console.log(textAreaData);
-    textAreaData.forEach((note) => {
-        return notes.push(note.value);
-    })
-    console.log(notes);
-    localStorage.setItem('notes', JSON.stringify(notes));//inserting data from local storage
-}
+  const textAreaData = document.querySelectorAll("textarea");
+  const notes = [];
+  // console.log(textAreaData);
+  textAreaData.forEach((note) => {
+    return notes.push(note.value);
+  });
+  console.log(notes);
+  localStorage.setItem("notes", JSON.stringify(notes)); //inserting data from local storage
+};
 
-const addNewNote = (text = '') => {
-    const note = document.createElement('div');
-    note.classList.add('Note');
-    const htmlData = `
+const addNewNote = (text = "") => {
+  const note = document.createElement("div");
+  note.classList.add("Note");
+  const htmlData = `
     <div class="note container">
     <div class="operation container">
         <button class="button edit"><i class="fa fa-pencil" aria-hidden="true"></i></button>
@@ -22,34 +22,36 @@ const addNewNote = (text = '') => {
         <textarea class=" textbox ${text ? "hidden" : ""}"></textarea>
     </div>
     </div>`;
-    note.insertAdjacentHTML('afterbegin', htmlData);
-    // console.log(note);
-    const editButton = note.querySelector('.edit');
-    const deleteButton = note.querySelector('.delete');
-    const mainDiv = note.querySelector('.main');
-    const textArea = note.querySelector('textarea');
-    
-    textArea.value = text;
-    mainDiv.innerHTML = text;
+  note.insertAdjacentHTML("afterbegin", htmlData);
+  // console.log(note);
+  const editButton = note.querySelector(".edit");
+  const deleteButton = note.querySelector(".delete");
+  const mainDiv = note.querySelector(".main");
+  const textArea = note.querySelector("textarea");
 
-    deleteButton.addEventListener('click', () => {
-        note.remove();
-        updateLSData();
-    })
-    editButton.addEventListener('click', () => {
-        mainDiv.classList.toggle('hidden');
-        textArea.classList.toggle('hidden');
-    })
+  textArea.value = text;
+  mainDiv.innerHTML = text;
 
-    textArea.addEventListener('change', (event) => {
-        const value = event.target.value;
-        mainDiv.innerHTML = value;
-        updateLSData();
-        // console.log(value);
-    })
+  deleteButton.addEventListener("click", () => {
+    note.remove();
+    updateLSData();
+  });
+  editButton.addEventListener("click", () => {
+    mainDiv.classList.toggle("hidden");
+    textArea.classList.toggle("hidden");
+  });
 
-    document.body.appendChild(note); //appends a node as a last child of the parent or node
+  textArea.addEventListener("change", (event) => {
+    const value = event.target.value;
+    mainDiv.innerHTML = value;
+    updateLSData();
+    // console.log(value);
+  });
+
+  document.body.appendChild(note); //appends a node as a last child of the parent or node
+};
+const notes = JSON.parse(localStorage.getItem("notes")); //getting data from local storage
+if (notes) {
+  notes.forEach((note) => addNewNote(note));
 }
-const notes = JSON.parse(localStorage.getItem('notes')); //getting data from local storage
-if (notes) { notes.forEach((note) => addNewNote(note)) };
-addButton.addEventListener('click', () => addNewNote());
+addButton.addEventListener("click", () => addNewNote());
